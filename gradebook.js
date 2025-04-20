c12637c6f55ffea591e8880c88ac3a407c5b6286
Fate127 <montraeramsey3@gmail.com>
2025-04-20 00:26:40 -0400

    Add files via upload

---
+++ gradebook.js
@@ -0,0 +1,13 @@
+function fetchGradeData() {
+  console.log("fetchGradeData called");
+  // This function will fetch data in future assignments
+}
+
+function populateGradebook() {
+  console.log("populateGradebook called");
+  // This function will dynamically add rows to the gradebook in future assignments
+}
+
+// Call the stubs to test
+fetchGradeData();
+populateGradebook();
